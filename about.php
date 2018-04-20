6734f3f18b95ed9533142a541681d2b25db3fcbb
cwk5181 <38560057+cwk5181@users.noreply.github.com>
2018-04-20 20:39:33 +0800

    about 1.0.1

---
+++ about.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>About</title>
+	</head>
+	<body>
+		<ul>
+			<li><a href="index.php">HOME</a></li>
+			<li><a href="about.php">ABOUT</a></li>
+		</ul>
+		<h1>About</h1>		
+		<p>This page is under construction...</p>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -4,6 +4,10 @@
 		<title>Home</title>
 	</head>
 	<body>
+		<ul>
+			<li><a href="index.php"></a>Home</li>
+			<li><a href="about.php"></a>About</li>
+		</ul>
 		<h1>Home</h1>		
 	</body>
 </html>
